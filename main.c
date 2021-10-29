23f0388b7a970a02688244b21e2b378a28b5a6f5
Sarah Harrington <80339816+harrins@users.noreply.github.com>
2021-10-29 17:19:55 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,27 @@
+/*CEC 470  Computer Organization and Architecture
+  Instruction Decoder Project
+  Submitted by: Sarah Harrington, John-Patrick Mueller, AnnaMaria Summer
+  Created: 10/29/21  Last Edited:10/29/21*/
 
+  #define HALT_OPCODE 0x19
+
+  void fetchNextInstruction(void);
+  void executeInstruction(void);
+
+  unsigned char memory[65536];
+  unsigned char ACC=0;
+  unsigned char IR=0;
+  unsiged int MAR=0;
+  unsigned int PC=0;
+
+  int main(int argc, CHAR* argv[])
+  {
+    /*execution loop. continue fetching and executing until PC points to a
+    HALT instruction*/
+    while(memory[PC] != HALT_OPCODE)
+    {
+      fetchNextInstruction();
+      executeInstruction();
+    }
+    return 0;
+  }
