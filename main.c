6c1de48b8138265d40c5d676537282772198d020
awatsummer <71294412+awatsummer@users.noreply.github.com>
2021-11-09 17:49:04 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -14,9 +14,12 @@
   unsigned int MAR=0;
   unsigned int PC=0;
 
-  int main(int argc, CHAR* argv[])
+  int main(int argc, char* argv[])
   {
-    /*execution loop. continue fetching and executing until PC points to a
+    
+		/* READ FROM FILE AND LOAD INTO MEMORY HERE */
+		
+		/*execution loop. continue fetching and executing until PC points to a
     HALT instruction*/
     while(memory[PC] != HALT_OPCODE)
     {
@@ -25,3 +28,98 @@
     }
     return 0;
   }
+	
+	void fetchNextInstruction()
+	{
+		// IR = next opcode
+		IR = memory[PC];
+		
+		// checking opcode
+		if((IR & 0x80) == 0x80) /* Mathematical Operation */
+		{
+			/* if first bit is 1, mathematical operation */
+			
+			/* JOHN PC INCREMENT CODE HERE */
+			
+		} else if ((IR & 0xf0) == 0) { /* Memory operation */
+			// if first 4 bits are 0000, memory operation
+			
+			/* SUMMER PC INCREMENT CODE HERE */
+			
+			switch(IR & 0x0F)
+			{
+
+				case 0x00: // 0000 0000
+					// store ACC [16-bit address]
+					PC += 3;
+					break;
+				
+				case 0x01: // 0000 0001
+					// store ACC (8-bit constant)
+					PC += 2;
+					break;
+				
+				case 0x02: // 0000 0010
+					// store ACC [MAR] | MAR is 16-bit address
+					PC++;
+					break;
+				
+				case 0x04: // 0000 0100
+					//store MAR [16-bit address]
+					PC += 3;
+					break;
+				
+				case 0x05: // 0000 0101
+					// store MAR (16-bit constant)
+					PC += 3;
+					break;
+				
+			}
+			
+		} else if ((IR & 0xF8) == 0x10) { /* Branch/Jump */
+			// if first 5 bits are 00010, branch/jump operation
+			
+			/* SARAH PC INCREMENT CODE HERE */
+			
+		} else { /* Special Opcode */
+			// else its a special opcode
+			
+		}		
+		
+		
+	}
+	
+	void executeInstruction()
+	{
+		
+		// checking opcode
+		if((IR & 0x80) == 0x80) /* Mathematical Operation */
+		{
+			/* if first bit is 1, mathematical operation */
+			
+			/* JOHN INSTRUCTION CODE HERE */
+			
+		} else if ((IR & 0xf0) == 0) { /* Memory operation */
+			// if first 4 bits are 0000, memory operation
+			
+			/* SUMMER INSTRUCTION CODE HERE */
+			
+		} else if ((IR & 0xF8) == 0x10) { /* Branch/Jump */
+			// if first 5 bits are 00010, branch/jump operation
+			
+			/* SARAH INSTRUCTION CODE HERE */
+			
+		} else { /* Special Opcode */
+			// else its a special opcode
+			
+		}	
+		
+	}
+	
+	
+	
+
+	
+	
+	
+	
